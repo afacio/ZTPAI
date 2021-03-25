02686c256363c68509dc1c5162c05324114f086b
afacio <46495474+afacio@users.noreply.github.com>
2021-03-25 11:05:07 +0100

    Create UserService.java

---
+++ src/main/java/ztpai/ztpai/manager/UserService.java
@@ -0,0 +1,43 @@
+package ztpai.ztpai.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import ztpai.ztpai.models.User;
+import ztpai.ztpai.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Iterable<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void fillDB() {
+        save(new User(1L, "Tytanic", "Tytanic", "Tytanic" ));
+        save(new User(2L, "Pulp Fiction", "Pulp Fiction", "Pulp Fiction"));
+    }
+}
