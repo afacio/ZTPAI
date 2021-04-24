134e1180cb9d552d6da79b11ee1a5824c030d233
Adam <46495474+afacio@users.noreply.github.com>
2021-04-24 11:49:53 +0200

    Update PostRepository.java

--- src/main/java/ztpai/ztpai/repository/PostRepository.java
+++ src/main/java/ztpai/ztpai/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package ztpai.ztpai.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ztpai.ztpai.models.Post;
 
 @Repository
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
