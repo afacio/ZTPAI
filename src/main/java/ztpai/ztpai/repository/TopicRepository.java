1ebd0c4b65bf4fcf09a243e685ae4a0f558a607d
Adam <46495474+afacio@users.noreply.github.com>
2021-04-24 11:49:49 +0200

    Update TopicRepository.java

--- src/main/java/ztpai/ztpai/repository/TopicRepository.java
+++ src/main/java/ztpai/ztpai/repository/TopicRepository.java
@@ -1,9 +1,9 @@
 package ztpai.ztpai.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ztpai.ztpai.models.Topic;
 
 @Repository
-public interface TopicRepository extends CrudRepository<Topic, Long> {
+public interface TopicRepository extends JpaRepository<Topic, Long> {
 }
