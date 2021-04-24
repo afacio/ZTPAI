ebb64dd204a76b79caf2bb9fbec1acb9987541b0
Adam <46495474+afacio@users.noreply.github.com>
2021-04-24 11:50:03 +0200

    Update Template.java

--- src/main/java/ztpai/ztpai/models/Template.java
+++ src/main/java/ztpai/ztpai/models/Template.java
@@ -8,15 +8,10 @@ public class Template {
 
     private LocalDate createDate;
     private LocalTime createTime;
-    private Long assignedBy;
 
     public Template() {
-    }
-
-    public Template(Long assignedBy) {
         this.createDate = LocalDate.now();
         this.createTime = LocalTime.now();
-        this.assignedBy = assignedBy;
     }
 
     public LocalDate getCreateDate() {
@@ -35,11 +30,5 @@ public class Template {
         this.createTime = createTime;
     }
 
-    public Long getAssignedBy() {
-        return assignedBy;
-    }
 
-    public void setAssignedBy(Long assignedBy) {
-        this.assignedBy = assignedBy;
-    }
 }
