5504deb9d8a069f0378221689a2458dec5842a63
Adam <46495474+afacio@users.noreply.github.com>
2021-05-28 17:40:11 +0200

    Update ZtpaiApplication.java

--- src/main/java/ztpai/ztpai/ZtpaiApplication.java
+++ src/main/java/ztpai/ztpai/ZtpaiApplication.java
@@ -2,8 +2,9 @@ package ztpai.ztpai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ZtpaiApplication {
 
     public static void main(String[] args) {
