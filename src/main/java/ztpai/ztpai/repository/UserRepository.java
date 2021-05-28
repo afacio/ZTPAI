bbc006021bede3b238b10201169576d2db5cf669
Adam <46495474+afacio@users.noreply.github.com>
2021-05-28 17:39:25 +0200

    Update UserRepository.java

--- src/main/java/ztpai/ztpai/repository/UserRepository.java
+++ src/main/java/ztpai/ztpai/repository/UserRepository.java
@@ -2,13 +2,19 @@ package ztpai.ztpai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ztpai.ztpai.models.User;
+import ztpai.ztpai.models.UserModel;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 
-    List<User> findAllByEmailOrderByNickName(String email);
+    UserModel findByEmail(String email);
+
+    UserModel findByUsername(String username);
+
+    Optional<UserModel> findByUserId(Long id);
+
+
 
 }
