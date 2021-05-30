2210040babff48b79d5ff4018bfbf10ac0055fa9
Adam <46495474+afacio@users.noreply.github.com>
2021-05-30 12:47:53 +0200

    Create Token.java

---
+++ src/main/java/ztpai/ztpai/models/Token.java
@@ -0,0 +1,42 @@
+package ztpai.ztpai.models;
+
+import org.springframework.security.core.userdetails.User;
+
+import javax.persistence.*;
+
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String value;
+
+    @OneToOne
+    private UserModel userModel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
+    }
+}
