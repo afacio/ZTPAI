916eacf5f9bba5c16cd0f88068ca317c5715f895
Adam <46495474+afacio@users.noreply.github.com>
2021-05-30 21:39:54 +0200

    Update UserService.java
    Dodanie do UserService obsługi wyjątków

--- src/main/java/ztpai/ztpai/services/UserService.java
+++ src/main/java/ztpai/ztpai/services/UserService.java
@@ -1,11 +1,11 @@
 package ztpai.ztpai.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import ztpai.ztpai.exception.user.UserExistsException;
+import ztpai.ztpai.exception.user.UserNotFoundException;
 import ztpai.ztpai.models.Token;
 import ztpai.ztpai.models.UserModel;
 import ztpai.ztpai.repository.TokenRepository;
@@ -30,24 +30,41 @@ public class UserService  {
         this.mailService = mailService;
     }
 
-    public Optional<UserModel> findById(Long id) {
-        //TODO gdy nie ma takiego id
-
-        return userRepository.findById(id);
-    }
-
     public Iterable<UserModel> getAll() {
         return userRepository.findAll();
     }
 
     public void addUser(UserModel user) {
-        //TODO istnieje już taki użytkownik
+        if(userRepository.findByEmail(user.getEmail()).isPresent() || userRepository.findByUsername(user.getUsername()).isPresent())
+            throw new UserExistsException();
+        else{
+            user.setPassword(encodePassword(user.getPassword()));
+            user.setRole("ROLE_USER");
+            user.setEnable(false);
+            userRepository.save(user);
+            sendToken(user);
+        }
+    }
 
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRole("ROLE_USER");
-        user.setEnable(false);
-        userRepository.save(user);
-        sendToken(user);
+    public void deleteById(Long id) {
+        userRepository.findById(id).ifPresentOrElse(userModel -> {userRepository.deleteById(id);}, () -> {throw new UserNotFoundException(id);});
+    }
+
+    public void updateUser(Long id, UserModel newUser){
+
+        UserModel oldUser = userRepository.findById(id).orElseThrow(() -> {throw new UserNotFoundException(id);});
+
+        if(newUser.getUsername() != null) oldUser.setUsername(newUser.getUsername());
+        if(newUser.getRole() != null) oldUser.setRole(newUser.getRole());
+        if(newUser.getEmail() != null) oldUser.setEmail(newUser.getEmail());
+        if(newUser.getForumsSet() != null) oldUser.setForumsSet(newUser.getForumsSet());
+        if(newUser.getPassword() != null)oldUser.setPassword(encodePassword(newUser.getPassword()));
+
+        userRepository.save(oldUser);
+    }
+
+    public UserModel getById(Long id){
+        return userRepository.findByUserId(id).orElseThrow(() -> new UserNotFoundException(id));
     }
 
     private void sendToken(UserModel userModel) {
@@ -58,7 +75,7 @@ public class UserService  {
         token.setUserModel(userModel);
 
         tokenRepository.save(token);
-
+        //TODO uniwersalizacja url
         String url="hhtp://localhost:8080/token?value=" + tokenValue;
 
         try {
@@ -69,11 +86,7 @@ public class UserService  {
 
     }
 
-    public void deleteById(Long id) {
-        //TODO gdy nie ma takiego id
-
-        userRepository.deleteById(id);
+    private String encodePassword(String password){
+        return passwordEncoder.encode(password);
     }
-
-
 }
