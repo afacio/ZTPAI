83e25b87edb444b62aafa23de62f2e6a96fd7794
Adam <46495474+afacio@users.noreply.github.com>
2021-05-30 12:47:50 +0200

    Update TestApi.java

--- src/main/java/ztpai/ztpai/controller/TestApi.java
+++ src/main/java/ztpai/ztpai/controller/TestApi.java
@@ -1,37 +1,44 @@
 package ztpai.ztpai.controller;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ztpai.ztpai.models.Token;
 import ztpai.ztpai.models.UserModel;
+import ztpai.ztpai.repository.TokenRepository;
+import ztpai.ztpai.repository.UserRepository;
 import ztpai.ztpai.services.UserService;
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
 
 @Controller
 public class TestApi {
 
-    private UserService userService;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
-    public TestApi(UserService userService){
+    public TestApi(UserService userService, UserRepository userRepository, TokenRepository tokenRepository){
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
     }
 
 
-    @GetMapping("/for-admin")
-    public String forAdmin(){
-        return "hello-admin";
-    }
-
-    @GetMapping("/for-user")
-    public String forUser(){
-        return "hello-user";
-    }
 
     @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hi there!";
+    public String hello(Principal principal, Model model) {
+        model.addAttribute("name", principal.getName());
+        Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        model.addAttribute("authorities", authorities);
+
+        return "hello";
     }
 
     @GetMapping("/sign-up")
@@ -43,6 +50,16 @@ public class TestApi {
     @PostMapping("/register")
     public String register(UserModel userModel){
         userService.addUser(userModel);
+
+        return "sign-up";
+    }
+
+    @GetMapping("/token")
+    public String token(@RequestParam String value){
+        Token token = tokenRepository.findByValue(value);
+        UserModel userModel = token.getUserModel();
+        userModel.setEnable(true);
+        userRepository.save(userModel);
 
         return "sign-up";
     }
