a9532b1371b965dafe38f2f8615554db9a82ed59
Adam <46495474+afacio@users.noreply.github.com>
2021-08-30 09:17:05 +0200

    Update UserService.java

--- src/main/java/ztpai/ztpai/services/UserService.java
+++ src/main/java/ztpai/ztpai/services/UserService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import ztpai.ztpai.exception.forum.ForumNotFoundException;
 import ztpai.ztpai.exception.user.UserExistsException;
 import ztpai.ztpai.exception.user.UserNotFoundException;
 import ztpai.ztpai.models.Token;
@@ -12,6 +13,7 @@ import ztpai.ztpai.repository.TokenRepository;
 import ztpai.ztpai.repository.UserRepository;
 
 import javax.mail.MessagingException;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,12 +21,14 @@ import java.util.UUID;
 public class UserService  {
 
     private final UserRepository userRepository;
+    private final ForumService forumService;
     private final PasswordEncoder passwordEncoder;
     private final TokenRepository tokenRepository;
     private final MailService mailService;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenRepository tokenRepository, MailService mailService) {
+    public UserService(UserRepository userRepository, ForumService forumService, PasswordEncoder passwordEncoder, TokenRepository tokenRepository, MailService mailService) {
         this.userRepository = userRepository;
+        this.forumService = forumService;
         this.passwordEncoder = passwordEncoder;
         this.tokenRepository = tokenRepository;
         this.mailService = mailService;
@@ -55,20 +59,37 @@ public class UserService  {
     }
 
     public void deleteUser(Long userId) {
+        //TODO nie działa usuwanie dla nowo utworzonych userów przez metode post
         userRepository.findById(userId).ifPresentOrElse(userModel -> {userRepository.deleteById(userId);}, () -> {throw new UserNotFoundException(userId);});
     }
 
     public void updateUser(Long id, UserModel newUser){
-
+//TODO doać aby nie można było podmieniać danych na już istniejące
         UserModel oldUser = userRepository.findById(id).orElseThrow(() -> {throw new UserNotFoundException(id);});
 
         if(newUser.getUsername() != null) oldUser.setUsername(newUser.getUsername());
         if(newUser.getRole() != null) oldUser.setRole(newUser.getRole());
         if(newUser.getEmail() != null) oldUser.setEmail(newUser.getEmail());
-        if(newUser.getForumsSet() != null) oldUser.setForumsSet(newUser.getForumsSet());
+        //if(newUser.getForumsSet() != null) oldUser.setForumsSet(newUser.getForumsSet());
         if(newUser.getPassword() != null)oldUser.setPassword(encodePassword(newUser.getPassword()));
+        if(newUser.getForumIdList() != null)oldUser.setForumIdList(newUser.getForumIdList());
 
         userRepository.save(oldUser);
+    }
+
+    public void addForumIdToList(Long userId, Long forumId){
+        userRepository.findById(userId).ifPresentOrElse(user -> {
+                    forumService.findById(forumId).ifPresentOrElse(forum -> {
+                        user.setForumIdListElement(forumId);
+                        userRepository.save(user);
+                    },() -> {throw new ForumNotFoundException(forumId);});},
+                () -> {throw new UserNotFoundException(userId);});
+    }
+
+    public List<Long> getUserForumIdList(Long userId){
+        //TODO gdy lista jest pusta zrobić obsługę wyjątków
+        UserModel userModel = userRepository.findById(userId).orElseThrow(() -> {throw new UserNotFoundException(userId);});
+        return userModel.getForumIdList();
     }
 
     private void sendToken(UserModel userModel) {
