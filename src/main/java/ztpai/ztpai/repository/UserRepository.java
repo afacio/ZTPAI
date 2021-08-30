42d16a6a06adebcb345c39d0af219b4873303cee
Adam <46495474+afacio@users.noreply.github.com>
2021-08-30 09:17:10 +0200

    Update UserRepository.java

--- src/main/java/ztpai/ztpai/repository/UserRepository.java
+++ src/main/java/ztpai/ztpai/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ztpai.ztpai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Repository;
 import ztpai.ztpai.models.UserModel;
 
